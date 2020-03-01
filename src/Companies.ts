1786841a1443a23fb6ed26bc52d86c19a86c6f1a
MarcMogdanz <info@minekayama.de>
2020-03-01 01:26:47 +0100

    add more companies

--- src/Companies.ts
+++ src/Companies.ts
@@ -12,7 +12,14 @@ const zapHosting = new Company(
   [BillingModel.CONTRACT, BillingModel.PREPAID],
   7,
   [PriceModel.RAM, PriceModel.SLOTS],
-  [PaymentMethod.PAYPAL, PaymentMethod.PAYSAFECARD],
+  [
+    PaymentMethod.PAYPAL,
+    PaymentMethod.PAYSAFECARD,
+    PaymentMethod.BITCOIN,
+    PaymentMethod.CREDIT_CARD,
+    PaymentMethod.SOFORT,
+    PaymentMethod.GIROPAY,
+  ],
   DefaultPort.EXTRA_CHARGE,
   DDoSProtection.FREE,
   null,
@@ -29,31 +36,93 @@ const nitrado = new Company(
     PaymentMethod.PAYSAFECARD,
     PaymentMethod.PAYPAL,
     PaymentMethod.WIRE_TRANSFER,
+    PaymentMethod.CREDIT_CARD,
+    PaymentMethod.SOFORT,
+    PaymentMethod.GIROPAY,
   ],
   DefaultPort.FREE,
   DDoSProtection.FREE,
   Database.FREE,
-  "/zap_hosting.png",
+  "/nitrado.png",
   "https://nitrado.net",
 );
 
-const test = new Company(
-  "TEST",
-  null,
+const gportal = new Company(
+  "gportal",
+  [BillingModel.PREPAID],
   3,
   [PriceModel.SLOTS],
   [
-    PaymentMethod.PAYSAFECARD,
+    PaymentMethod.SOFORT,
+    PaymentMethod.CREDIT_CARD,
     PaymentMethod.PAYPAL,
-    PaymentMethod.WIRE_TRANSFER,
+    PaymentMethod.PAYSAFECARD,
   ],
   DefaultPort.FREE,
-  DDoSProtection.EXTRA_CHARGE,
+  DDoSProtection.FREE,
   null,
-  "/zap_hosting.png",
-  "https://google.com",
+  "/gportal.png",
+  "https://g-portal.com",
 );
 
-const AllCompanies = [zapHosting, nitrado, test];
+const hostUnlimited = new Company(
+  "Host Unlimited",
+  [BillingModel.PREPAID],
+  30,
+  [PriceModel.RAM],
+  [
+    PaymentMethod.WIRE_TRANSFER,
+    PaymentMethod.SOFORT,
+    PaymentMethod.PAYSAFECARD,
+    PaymentMethod.PAYPAL,
+    PaymentMethod.BITCOIN,
+  ],
+  DefaultPort.FREE,
+  DDoSProtection.FREE,
+  Database.FREE,
+  "/host_unlimited.png",
+  "https://host-unlimited.de",
+);
 
-export { AllCompanies };
+const mcHost24 = new Company(
+  "MC-HOST24",
+  [BillingModel.PREPAID],
+  3,
+  [PriceModel.RAM],
+  [PaymentMethod.PAYSAFECARD, PaymentMethod.SOFORT, PaymentMethod.PAYPAL],
+  DefaultPort.FREE,
+  DDoSProtection.FREE,
+  Database.FREE,
+  "/mc-host24.png",
+  "https://mc-host24.de",
+);
+
+const deinServerHost = new Company(
+  "DeinServerHost",
+  [BillingModel.PREPAID],
+  30,
+  [PriceModel.RAM],
+  [
+    PaymentMethod.PAYSAFECARD,
+    PaymentMethod.PAYPAL,
+    PaymentMethod.SOFORT,
+    PaymentMethod.WIRE_TRANSFER,
+    PaymentMethod.GIROPAY,
+    PaymentMethod.CREDIT_CARD,
+    PaymentMethod.BITCOIN,
+  ],
+  DefaultPort.FREE,
+  DDoSProtection.FREE,
+  Database.FREE,
+  "/dein_server_host.svg",
+  "https://deinserverhost.de",
+);
+
+export const AllCompanies = [
+  zapHosting,
+  nitrado,
+  gportal,
+  hostUnlimited,
+  mcHost24,
+  deinServerHost,
+];
